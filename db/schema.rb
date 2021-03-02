36e892392859680c5f9346b8e18cf67eb50da0c7
Daniel Skupien <info@danielskupien.de>
2021-03-02 07:49:47 +0100

    added Projects controller and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_064041) do
+ActiveRecord::Schema.define(version: 2021_03_02_064727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.string "punchline"
+    t.string "description"
+    t.string "video"
+    t.string "image"
+    t.string "category"
+    t.integer "investment_goal"
+    t.date "investment_goal_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,5 @@ ActiveRecord::Schema.define(version: 2021_03_02_064041) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
