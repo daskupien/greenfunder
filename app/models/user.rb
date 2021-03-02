fe5f90860c1cc98bb5c41eab0dce1b3e18e32e91
Daniel Skupien <info@danielskupien.de>
2021-03-02 07:43:55 +0100

    added user columns and generated user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :projects, dependent: :destroy
+  has_many :investments, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
