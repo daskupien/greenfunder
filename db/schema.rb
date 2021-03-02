fa8390efc914d1e7c6f59894fb7d1db4a6da6537
Daniel Skupien <info@danielskupien.de>
2021-03-02 08:07:04 +0100

    user can browse projects at projects#index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_065106) do
+ActiveRecord::Schema.define(version: 2021_03_02_065424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,16 @@ ActiveRecord::Schema.define(version: 2021_03_02_065106) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
+  create_table "updates", force: :cascade do |t|
+    t.string "title"
+    t.string "images"
+    t.text "content"
+    t.bigint "project_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["project_id"], name: "index_updates_on_project_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,4 +69,5 @@ ActiveRecord::Schema.define(version: 2021_03_02_065106) do
   add_foreign_key "investments", "projects"
   add_foreign_key "investments", "users"
   add_foreign_key "projects", "users"
+  add_foreign_key "updates", "projects"
 end
