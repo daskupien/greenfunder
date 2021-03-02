99523fcf06d78794761338a00afaa022313497ac
Daniel Skupien <info@danielskupien.de>
2021-03-02 14:41:47 +0100

    user can create investment

--- app/controllers/investments_controller.rb
+++ app/controllers/investments_controller.rb
@@ -1,2 +1,24 @@
 class InvestmentsController < ApplicationController
+  def new
+    @investment = Investment.new
+    @project = Project.find(params[:project_id])
+  end
+
+  def create
+    @investment = Investment.new(investment_params)
+    project = Project.find(params[:project_id])
+    @investment.user = current_user
+    @investment.project = project
+    if @investment.save
+      redirect_to project
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def investment_params
+    params.require(:investment).permit(:comment, :investment_amount)
+  end
 end
