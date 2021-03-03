ee741486ebed1857ae438713ef912dd16ee3de72
Daniel Skupien <info@danielskupien.de>
2021-03-03 15:13:36 +0100

    trying to add process bar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @investments = Investment.where(user: current_user)
+    @investments.each do |investment|
+      current_investment_sum = investment.project.current_investment_sum
+      investment_goal = investment.project.investment_goal
+      @investment_percentage = current_investment_sum.to_f / investment_goal * 100.0
+      investment.investment_percentage = @investment_percentage
+    end
   end
 end
