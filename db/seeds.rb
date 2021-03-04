ee11f384f41ee707bd9f306d39f8241901e260ce
Daniel Skupien <info@danielskupien.de>
2021-03-04 11:41:34 +0100

    added seeds for category search

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,14 +22,14 @@ daniel = User.create!(email: 'daniel@greenfunder.de', password: '123456', first_
 puts 'creating projects'
 
 project1 = Project.new(
-    name: 'Project 1',
-    punchline: 'amazing punchline',
+    name: '#1 Trees',
+    punchline: 'Help planting a forest in our neighborhood',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra sit amet. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Etiam erat velit scelerisque in dictum non. A condimentum vitae sapien pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis. Convallis posuere morbi leo urna molestie. Mattis rhoncus urna neque viverra justo nec ultrices. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Mauris ultrices eros in cursus turpis massa. Quisque egestas diam in arcu cursus euismod. Pretium quam vulputate dignissim suspendisse in est ante in. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. In nulla posuere sollicitudin aliquam ultrices sagittis. Id venenatis a condimentum vitae sapien pellentesque habitant. Euismod nisi porta lorem mollis aliquam ut porttitor. Tellus rutrum tellus pellentesque eu.',
-    video: 'https://www.youtube.com/watch?v=XDdpfWgS4kM',
-    category: 'gardening',
+    video: 'https://www.youtube.com/watch?v=HPJKxAhLw5I',
+    category: 'planting',
     investment_goal: 10000,
     current_investment_sum: 0,
-    address: 'Berlin',
+    address: 'Frankfurt am Main',
     user_id: begana.id,
 )
 file = URI.open('https://res.cloudinary.com/rentmylife/image/upload/v1614783406/assets/eastern-white-pine-trees-4125586-04-3b9e60ad3ac14efba38ce0ec72b6770d_qjvux8.jpg')
@@ -37,14 +37,14 @@ project1.image.attach(io: file, filename: 'seed1.jpg', content_type: 'image/jpg'
 project1.save
 
 project2 = Project.new(
-    name: 'Project 2',
-    punchline: 'amazing punchline',
+    name: '#2 Solar Berlin',
+    punchline: "Let's bring a solar panel on every roof in Berlin",
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra sit amet. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Etiam erat velit scelerisque in dictum non. A condimentum vitae sapien pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis. Convallis posuere morbi leo urna molestie. Mattis rhoncus urna neque viverra justo nec ultrices. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Mauris ultrices eros in cursus turpis massa. Quisque egestas diam in arcu cursus euismod. Pretium quam vulputate dignissim suspendisse in est ante in. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. In nulla posuere sollicitudin aliquam ultrices sagittis. Id venenatis a condimentum vitae sapien pellentesque habitant. Euismod nisi porta lorem mollis aliquam ut porttitor. Tellus rutrum tellus pellentesque eu.',
-    video: 'https://www.youtube.com/watch?v=XDdpfWgS4kM',
-    category: 'gardening',
+    video: 'https://www.youtube.com/watch?v=LRPy8UZv9V0',
+    category: 'energy',
     investment_goal: 10000,
     current_investment_sum: 0,
-    address: 'Munich',
+    address: 'Berlin',
     user_id: loretta.id,
 )
 file = URI.open('https://res.cloudinary.com/rentmylife/image/upload/v1614783937/assets/bee-146810332_sszmax.jpg')
@@ -52,11 +52,11 @@ project2.image.attach(io: file, filename: 'seed2.jpg', content_type: 'image/jpg'
 project2.save
 
 project3 = Project.new(
-    name: 'Project 3',
-    punchline: 'amazing punchline',
+    name: '#3 Beefy our garden',
+    punchline: 'Help us grow bees to save the nearby plants',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra sit amet. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Etiam erat velit scelerisque in dictum non. A condimentum vitae sapien pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis. Convallis posuere morbi leo urna molestie. Mattis rhoncus urna neque viverra justo nec ultrices. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Mauris ultrices eros in cursus turpis massa. Quisque egestas diam in arcu cursus euismod. Pretium quam vulputate dignissim suspendisse in est ante in. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. In nulla posuere sollicitudin aliquam ultrices sagittis. Id venenatis a condimentum vitae sapien pellentesque habitant. Euismod nisi porta lorem mollis aliquam ut porttitor. Tellus rutrum tellus pellentesque eu.',
-    video: 'https://www.youtube.com/watch?v=XDdpfWgS4kM',
-    category: 'gardening',
+    video: 'https://www.youtube.com/watch?v=EXGxUCxrL04',
+    category: 'animals',
     investment_goal: 10000,
     current_investment_sum: 0,
     address: 'Dresden',
@@ -67,11 +67,11 @@ project3.image.attach(io: file, filename: 'seed3.jpg', content_type: 'image/jpg'
 project3.save
 
 project4 = Project.new(
-    name: 'Project 4',
-    punchline: 'amazing punchline',
+    name: '#4 New packaging solution for fruits',
+    punchline: 'We will make the world much less platic poluted',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo donec enim diam vulputate ut pharetra sit amet. Malesuada nunc vel risus commodo viverra maecenas accumsan lacus. Habitasse platea dictumst quisque sagittis purus sit amet volutpat. Etiam erat velit scelerisque in dictum non. A condimentum vitae sapien pellentesque habitant morbi tristique. Ac tincidunt vitae semper quis. Convallis posuere morbi leo urna molestie. Mattis rhoncus urna neque viverra justo nec ultrices. Auctor neque vitae tempus quam pellentesque nec nam aliquam sem. Tincidunt nunc pulvinar sapien et ligula ullamcorper. Mauris ultrices eros in cursus turpis massa. Quisque egestas diam in arcu cursus euismod. Pretium quam vulputate dignissim suspendisse in est ante in. Consequat ac felis donec et odio pellentesque diam volutpat commodo. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus. In nulla posuere sollicitudin aliquam ultrices sagittis. Id venenatis a condimentum vitae sapien pellentesque habitant. Euismod nisi porta lorem mollis aliquam ut porttitor. Tellus rutrum tellus pellentesque eu.',
-    video: 'https://www.youtube.com/watch?v=XDdpfWgS4kM',
-    category: 'gardening',
+    video: 'https://www.youtube.com/watch?v=9vFJHQxPbDY',
+    category: 'plastic alternatives',
     investment_goal: 10000,
     current_investment_sum: 0,
     address: 'Hamburg',
