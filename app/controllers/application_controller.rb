5569aeba9a4b008fa6016ed9292f2f083871386e
Jonas Kozik <jonas.kozik@gmail.com>
2021-03-05 15:12:26 +0100

    stripe fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  include Pundit
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
