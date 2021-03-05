90d0ea2396a20e46848ad1248d5dde67ba097ddd
Loretta Matzke <lorettamatzke@gmail.com>
2021-03-05 11:58:27 +0100

    filter  on home cards, hover function, moved the bar, button full width

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @investments = Investment.where(user: current_user)
+    @projects = Project.where(user: current_user)
   end
 end
